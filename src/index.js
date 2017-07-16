c4ecc8631e1d45e18da19975bd79bb71342b6ad8
Andrew <Sir.Serfer@gmail.com>
2017-07-16 17:15:55 +0300

    Add geolocarion by Google Maps API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import DarkSkyApi from 'dark-sky-api';
-
+import Chart from 'chart.js';
 
 DarkSkyApi.apiKey = '21e22ddd92fa2e53acb69b4a9391cedc';
 DarkSkyApi.units = 'si'; // default 'us'
@@ -33,4 +33,43 @@ DarkSkyApi.loadForecast()
       document.getElementsByClassName("day-weather")[i].innerHTML = `<b>${Math.round(result.daily.data[i+1].temperatureMax)}°</b>       ${Math.round(result.daily.data[i+1].temperatureMin)}°`;
     }
 });
+
+// new Chart(document.getElementById("line-chart"), {
+//   type: 'line',
+//   data: {
+//     labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
+//     datasets: [{ 
+//         data: [86,114,106,106,1000,111,500,221,783,2478],
+//         label: "Africa",
+//         borderColor: "#3e95cd",
+//         fill: true
+//       }
+//     ]
+//   },
+//   options: {
+//     title: {
+//       display: true,
+//       text: 'World population per region (in millions)'
+//     }
+//   }
+// });
+
+// AIzaSyCC1Xutodrcal3u-x_KHhUh6kk0OAeeu30
+
+
+DarkSkyApi.loadPosition()
+  .then(pos => {
+    let latitude = pos.latitude;
+    let longitude = pos.longitude;
+    console.log(latitude + " " + longitude);
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyCC1Xutodrcal3u-x_KHhUh6kk0OAeeu30`;
+    fetch(url)
+      .then((resp) => resp.json())
+      .then(function(data) {
+        document.getElementsByClassName("city")[0].innerHTML = data.results[3].formatted_address;
+        console.log(data.results[3].formatted_address);
+      });
+  });
+
+
 
